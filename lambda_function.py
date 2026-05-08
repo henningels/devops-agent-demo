8eb11b6ab2bb5b8db2f41b354f93f9338f9c810a
Henning Els <henningels@gmail.com>
2026-05-08 16:22:37 -0700

    Fix Lambda handler name and zip filename

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ import boto3
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 table = dynamodb.Table('Books')
 
-def lambda_handler(event, context):
+def handler(event, context):
     response = table.scan()
     books = response.get('Items', [])
     for book in books:
